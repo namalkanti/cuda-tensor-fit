3f5a5bd2795e333acd6f02b431a280a26a3b6d81
neji49 <neji49@gmail.com>
2013-05-27 01:27:11 -0700

    Getting rid of object files

--- fit_tensor_cuda.cu
+++ fit_tensor_cuda.cu
@@ -6,31 +6,31 @@
 //clones double array and copies to gpu
 double* cuda_double_copy(double* arr, size_t len){
     double* carr;
-    cudamalloc(&carr, sizeof(double) * len);
-    cudamemcpy(carr, arr, sizeof(double) * len, cudamemcpyhosttodevice);
+    cudaMalloc(&carr, sizeof(double) * len);
+    cudaMemcpy(carr, arr, sizeof(double) * len, cudaMemcpyHostToDevice);
     return carr;
 }
 
 //clones double array and copies to host 
 double* cuda_double_return(double* carr, size_t len){
     double* arr = (double*) malloc(sizeof(double) * len);
-    cudamemcpy(arr, carr, sizeof(double) * len, cudamemcpydevicetohost);
+    cudaMemcpy(arr, carr, sizeof(double) * len, cudaMemcpyDeviceToHost);
     return arr;
 }
 
 //allocates space for a double array on the device
 void cudal_double_alloc(double* ptr, int len){
-    cudamalloc(&ptr, len);
+    cudaMalloc(&ptr, len);
 }
 
 //frees double device memory
 void free_cuda(double* ptr){
-    cudafree(ptr);
+    cudaFree(ptr);
 }
 
 //kernel to take entire array and run cutoff log function
 __global__ void cutoff_log_kernel(double* input, double* output, double min_signal, size_t len){
-    int tid = blockidx.x;
+    int tid = blockIdx.x;
     if (input[tid] < min_signal){
         output[tid] = log(min_signal);
     }
@@ -41,8 +41,8 @@ __global__ void cutoff_log_kernel(double* input, double* output, double min_sign
 
 //kernel to take entire array and exp it
 __global__ void exp_kernel(double* input, double* output, size_t len){
-    int tid = blockidx.x;
-    output[tid] = pow(m_e, input[tid]);
+    int tid = blockIdx.x;
+    output[tid] = pow(M_E, input[tid]);
 }
 
 //function that take in a complete signal matrix and fits it, cuda version
