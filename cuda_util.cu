c62309a5d24b6e84cbebc4f807b0f21ddb8eb7ae
Niraj Amalkanti <neji49@gmail.com>
2013-10-01 09:29:15 -0700

    Adding debugging to cuda_util.cu

--- cuda_util.cu
+++ cuda_util.cu
@@ -73,8 +73,13 @@ void exp_cuda(float* input, int array_length){
     int blocks_in_grid = padded_array->current_length/ WARP_SIZE;
     exp_kernel<<<blocks_in_grid, WARP_SIZE>>>(device_array);
     padded_array->values = cuda_float_return_from_gpu(device_array, padded_array->current_length);
-    input = get_array_from_padded_array(padded_array);
+    output_array = get_array_from_padded_array(padded_array);
+    int i;
+    for(i = 0; i < array_length; i++){
+        input[i] = output_array[i];
+    }
     free_cuda_memory(device_array);
     free_padded_array(padded_array);
+    free(output_array);
 }
 
