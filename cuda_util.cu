8af59aa08fb6fbe3658721f3baa6026307b899ac
neji49 <neji49@gmail.com>
2013-10-29 13:23:03 -0700

    Actually assigned array to matrix struct

--- cuda_util.cu
+++ cuda_util.cu
@@ -148,6 +148,7 @@ matrix* cuda_matrix_dot(matrix* matrix1, matrix* matrix2){
     double* result_matrix_data = (double *) malloc(sizeof(double) * matrix1->rows * matrix2->columns);
     result_matrix->rows = matrix1->rows;
     result_matrix->columns = matrix2->columns;
+    result_matrix->data = result_matrix_data;
     get_matrix_from_gpu_and_convert_from_fortran(gpu_output, result_matrix);
     //cudaFree(gpu_array1);
     //cudaFree(gpu_array2);
