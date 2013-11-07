fc83ebe8503d9e9f38f42bf706d9741e62c85df6
neji49 <neji49@gmail.com>
2013-11-07 12:39:37 -0800

    Code compiles but have to add test

--- cuda_test.c
+++ cuda_test.c
@@ -198,6 +198,11 @@ int main(){
         return CU_get_error();
     }
 
+    if ((NULL == CU_add_test(cuda_suite, "Matrix weighting test", test_matrix_weighter))){
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
     CU_basic_set_mode(CU_BRM_VERBOSE);
     CU_basic_run_tests();
     CU_cleanup_registry();
