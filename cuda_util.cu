79f658d82a3b8102360d71c5784e6fd545fc18d8
neji49 <neji49@gmail.com>
2013-12-10 09:52:12 -0800

    Updating cuda code

--- cuda_util.cu
+++ cuda_util.cu
@@ -7,8 +7,19 @@ extern "C" {
 #include "BatchedSolver/solve.h"
 #define IDX2C(i, j, ld) ((j)*(ld)+(i))
 
+//Helper function declarations
+double* convert_matrix_to_fortran_and_load_to_gpu(matrix* mat);
+void get_matrix_from_gpu_and_convert_from_fortran(double* gpu_pointer, matrix* mat);
+
+//Kernel declarations
+__global__ void cutoff_log_kernel(double* device_array, double min_signal);
+__global__ void exp_kernel(double* cuda_array);
+__global__ void weighting_kernel (double* matrices, double* weights); 
+__global__ void weighting_kernel_transposed(double* matrices, double* weights); 
+
+
+
 extern "C"
-//clones double array and copies to gpu
 double* cuda_double_copy_to_gpu(double* local_array, int array_length){
     double* cuda_array;
     cudaMalloc(&cuda_array, sizeof(double) * array_length);
@@ -17,7 +28,6 @@ double* cuda_double_copy_to_gpu(double* local_array, int array_length){
 }
 
 extern "C"
-//clones double array and copies to host 
 double* cuda_double_return_from_gpu(double* cuda_array, int array_length){
     double* result_array = (double *) malloc(sizeof(double) * array_length);
     cudaMemcpy(result_array, cuda_array, sizeof(double) * array_length, cudaMemcpyDeviceToHost);
@@ -25,30 +35,16 @@ double* cuda_double_return_from_gpu(double* cuda_array, int array_length){
 }
 
 extern "C"
-//allocates space for a double array on the device
 void cuda_double_allocate(double* pointer, int pointer_length){
     cudaMalloc(&pointer, pointer_length);
 }
 
 extern "C"
-//frees double device memory
 void free_cuda_memory(double* pointer){
     cudaFree(pointer);
 }
 
-//kernel to take entire array and run cutoff log function
-__global__ void cutoff_log_kernel(double* device_array, double min_signal){
-    int thread_id = blockIdx.x * blockDim.x + threadIdx.x;
-    if (device_array[thread_id] < min_signal){
-        device_array[thread_id] = logf(min_signal);
-    }
-    else{
-        device_array[thread_id] = logf(device_array[thread_id]);
-    }
-}
-
 extern "C"
-//Function to launch cutoff log kernel
 double* cutoff_log_cuda(double* input, double min_signal, int array_length){
     padded_double_array* padded_array = pad_array(input, array_length, WARP_SIZE);
     double* device_array = cuda_double_copy_to_gpu(padded_array->values, padded_array->current_length);
@@ -61,14 +57,7 @@ double* cutoff_log_cuda(double* input, double min_signal, int array_length){
     return result_array;
 }
 
-//kernel to take entire array and exp it
-__global__ void exp_kernel(double* cuda_array){
-    int thread_id = blockIdx.x * blockDim.x + threadIdx.x;
-    cuda_array[thread_id] = expf(cuda_array[thread_id]);
-}
-
 extern "C"
-//Kernel catapult
 double* exp_cuda(double* input, int array_length){
     padded_double_array* padded_array = pad_array(input, array_length, WARP_SIZE);
     double* device_array = cuda_double_copy_to_gpu(padded_array->values, padded_array->current_length);
@@ -80,6 +69,72 @@ double* exp_cuda(double* input, int array_length){
     free_padded_array(padded_array);
     return output_array;
 }
+
+extern "C"
+matrix* cuda_matrix_dot(matrix* matrix1, matrix* matrix2){
+    cublasStatus_t status;
+    cublasHandle_t handle;
+    status = cublasCreate(&handle);
+    if ( status != CUBLAS_STATUS_SUCCESS ) {
+        puts("Failed to retrieve cublas handle.");
+    }
+    double* gpu_array1 = convert_matrix_to_fortran_and_load_to_gpu(matrix1);
+    double* gpu_array2 = convert_matrix_to_fortran_and_load_to_gpu(matrix2);
+    double* gpu_output;
+    cudaMalloc(&gpu_output, sizeof(double)* matrix1->rows * matrix2->columns);
+    const double alpha = 1;
+    const double beta = 0;
+    status = cublasDgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, matrix1->rows, matrix2->columns, matrix1->columns, 
+            &alpha, gpu_array1, matrix1->rows, gpu_array2, matrix2->rows, &beta, gpu_output, matrix1->rows);
+    if ( status != CUBLAS_STATUS_SUCCESS ) {
+        puts("Call to cublas function failed.");
+    }
+    matrix* result_matrix = (matrix*) malloc(sizeof(matrix));
+    double* result_matrix_data = (double*) malloc(sizeof(double) * matrix1->rows * matrix2->columns);
+    result_matrix->rows = matrix1->rows;
+    result_matrix->columns = matrix2->columns;
+    result_matrix->data = result_matrix_data;
+    get_matrix_from_gpu_and_convert_from_fortran(gpu_output, result_matrix);
+    cudaFree(gpu_array1);
+    cudaFree(gpu_array2);
+    return result_matrix;
+}
+    
+extern "C"
+void matrix_weighter (double* matrices, double* weights, int rows, int columns, int length, bool trans) {
+    dim3 grid, block;
+    int weight_length;
+    grid.x = length;
+    block.x = columns;
+    block.y = rows;
+    if ( false == trans ) {
+        weight_length = columns;
+    }
+    else {
+        weight_length = rows;
+    }
+    double* gpu_matrices = cuda_double_copy_to_gpu(matrices, rows * columns * length);
+    double* gpu_weights = cuda_double_copy_to_gpu(weights, weight_length);
+    if (false == trans){
+        weighting_kernel<<<grid, block>>>(gpu_matrices, gpu_weights);
+    }
+    else {
+        weighting_kernel_transposed<<<grid, block>>>(gpu_matrices, gpu_weights);
+    }
+    cudaMemcpy(matrices, gpu_matrices, sizeof(double) * rows* columns * length, cudaMemcpyDeviceToHost);
+    cudaFree(gpu_matrices);
+    cudaFree(gpu_weights);
+}
+
+extern "C"
+double* cuda_fitter(matrix const* design_matrix, matrix const* weights, double const* signal, int signal_length){
+}
+
+extern "C"
+void decompose_tensors(double const* tensors, tensor** tensor_output){
+}
+
+//Helper functions
 
 /*Converts matrix to the data format fortran uses for CUBLAS and loads to GPU
   Returns pointer to array on GPU.*/
@@ -125,77 +180,39 @@ void get_matrix_from_gpu_and_convert_from_fortran(double* gpu_pointer, matrix* m
     free(intermediate_matrix);
 }
 
-extern "C"
-//Returns the dot product of the two matrices, does calculations on the GPU
-matrix* cuda_matrix_dot(matrix* matrix1, matrix* matrix2){
-    cublasStatus_t status;
-    cublasHandle_t handle;
-    status = cublasCreate(&handle);
-    if ( status != CUBLAS_STATUS_SUCCESS ) {
-        puts("Failed to retrieve cublas handle.");
+//Kernels
+
+//kernel to take entire array and run cutoff log function
+__global__ void cutoff_log_kernel(double* device_array, double min_signal){
+    int thread_id = blockidx.x * blockdim.x + threadidx.x;
+    if (device_array[thread_id] < min_signal){
+        device_array[thread_id] = logf(min_signal);
     }
-    double* gpu_array1 = convert_matrix_to_fortran_and_load_to_gpu(matrix1);
-    double* gpu_array2 = convert_matrix_to_fortran_and_load_to_gpu(matrix2);
-    double* gpu_output;
-    cudaMalloc(&gpu_output, sizeof(double)* matrix1->rows * matrix2->columns);
-    const double alpha = 1;
-    const double beta = 0;
-    status = cublasDgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, matrix1->rows, matrix2->columns, matrix1->columns, 
-            &alpha, gpu_array1, matrix1->rows, gpu_array2, matrix2->rows, &beta, gpu_output, matrix1->rows);
-    if ( status != CUBLAS_STATUS_SUCCESS ) {
-        puts("Call to cublas function failed.");
+    else{
+        device_array[thread_id] = logf(device_array[thread_id]);
     }
-    matrix* result_matrix = (matrix*) malloc(sizeof(matrix));
-    double* result_matrix_data = (double*) malloc(sizeof(double) * matrix1->rows * matrix2->columns);
-    result_matrix->rows = matrix1->rows;
-    result_matrix->columns = matrix2->columns;
-    result_matrix->data = result_matrix_data;
-    get_matrix_from_gpu_and_convert_from_fortran(gpu_output, result_matrix);
-    cudaFree(gpu_array1);
-    cudaFree(gpu_array2);
-    return result_matrix;
 }
 
-//Kernel for weighting the matrix.
+//kernel to take entire array and exp it
+__global__ void exp_kernel(double* cuda_array){
+    int thread_id = blockidx.x * blockdim.x + threadidx.x;
+    cuda_array[thread_id] = expf(cuda_array[thread_id]);
+}
+
+//kernel for weighting the matrix.
 __global__ void weighting_kernel (double* matrices, double* weights) {
-    int grid_index = blockIdx.x * blockDim.x * blockDim.y;
-    int block_index = blockDim.y * threadIdx.y + threadIdx.x;
+    int grid_index = blockidx.x * blockdim.x * blockdim.y;
+    int block_index = blockdim.y * threadidx.y + threadidx.x;
     int matrix_index = grid_index + block_index;
-    matrices[matrix_index] = matrices[matrix_index] * weights[threadIdx.x];
+    matrices[matrix_index] = matrices[matrix_index] * weights[threadidx.x];
 }
 
-//Kernel for weighting a transposed matrix.
+//kernel for weighting a transposed matrix.
 __global__ void weighting_kernel_transposed(double* matrices, double* weights) {
-    int grid_index = blockIdx.x * blockDim.x * blockDim.y;
-    int block_index = blockDim.y * threadIdx.y + threadIdx.x;
+    int grid_index = blockidx.x * blockdim.x * blockdim.y;
+    int block_index = blockdim.y * threadidx.y + threadidx.x;
     int matrix_index = grid_index + block_index;
-    int weighting_index = blockIdx.x * blockDim.y + threadIdx.y; 
+    int weighting_index = blockidx.x * blockdim.y + threadidx.y; 
     matrices[matrix_index] = matrices[matrix_index] * weights[weighting_index];
 }
 
-/*Matrix weighter will weight each row of a matrix by a value based on the trans flag that is passed in.
-  Function expects incoming values to be padded correctly.*/
-void matrix_weighter (double* matrices, double* weights, int rows, int columns, int length, bool trans) {
-    dim3 grid, block;
-    int weight_length;
-    grid.x = length;
-    block.x = columns;
-    block.y = rows;
-    if ( false == trans ) {
-        weight_length = columns;
-    }
-    else {
-        weight_length = rows;
-    }
-    double* gpu_matrices = cuda_double_copy_to_gpu(matrices, rows * columns * length);
-    double* gpu_weights = cuda_double_copy_to_gpu(weights, weight_length);
-    if (false == trans){
-        weighting_kernel<<<grid, block>>>(gpu_matrices, gpu_weights);
-    }
-    else {
-        weighting_kernel_transposed<<<grid, block>>>(gpu_matrices, gpu_weights);
-    }
-    cudaMemcpy(matrices, gpu_matrices, sizeof(double) * rows* columns * length, cudaMemcpyDeviceToHost);
-    cudaFree(gpu_matrices);
-    cudaFree(gpu_weights);
-}
--- cuda_util.h
+++ cuda_util.h
@@ -1,35 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "fit_tensor_util.h"
+#include "fit_tensor_opt.h"
 
 #define WARP_SIZE 32
 
 //Clones double array and copies to gpu
-double* cuda_double_copy_to_gpu(double* local_array, int array_length);
+double* cuda_double_copy_to_gpu(double const* local_array, int array_length);
 
 //Clones double array and copies to host 
-double* cuda_double_return_from_gpu(double* cuda_array, int array_length);
+double* cuda_double_return_from_gpu(double const* cuda_array, int array_length);
 
 //Allocates space for a double array on the device
 void cuda_double_allocate(double* pointer, int pointer_length);
 
 //Frees double device memory
-void free_cuda_memory(double* pointer);
+void free_cuda_memory(double const* pointer);
 
 //Kernel to take entire array and run cutoff log function
-double* cutoff_log_cuda(double* input, double min_signal, int array_length);
+double* cutoff_log_cuda(double const* input, double min_signal, int array_length);
 
 //Kernel to take entire array and exp it
-double* exp_cuda(double* input, int array_length);
+double* exp_cuda(double const* input, int array_length);
 
 //Wrapper for CUBLAS Dgemm call
-matrix* cuda_matrix_dot(matrix* matrix1, matrix* matrix2);
+matrix* cuda_matrix_dot(matrix const* matrix1, matrix const* matrix2);
 
 //Function weights matrix according to trans argument.
-void matrix_weighter (double* matrices, double* weights, int rows, int columns, int length, bool trans);
+void matrix_weighter (double* matrices, double const* weights, int rows, int columns, int length, bool trans);
 
 //Wrapper function to weight and fit the data
-double* cuda_fitter(matrix* design_matrix, matrix* weights, double * signal, int signal_length);
+double* cuda_fitter(matrix const* design_matrix, matrix const* weights, double const* signal, int signal_length);
 
 //Decomposes tensors and places them inside second argument.
-void decompose_tensors(double* tensors, tensor** tensor_output);
+void decompose_tensors(double const* tensors, tensor** tensor_output);
