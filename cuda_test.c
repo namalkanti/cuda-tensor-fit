1e35a519b935f13f5e407686244212c490d49aac
Niraj Amalkanti <neji49@gmail.com>
2014-04-27 21:26:32 -0700

    Cleared potential gpu memory leaks

--- cuda_test.c
+++ cuda_test.c
@@ -140,16 +140,22 @@ void test_process_matrix(void){
 //Test case for cuda fitter function
 void test_cuda_fitter(void){
     double test_design_data[] = {1, 3, 2, 4};
-    matrix* test_design_matrix = create_matrix(cuda_double_copy_to_gpu(test_design_data, 4), 2, 2);
+    double* gpu_design = cuda_double_copy_to_gpu(test_design_data, 4);
+    matrix* test_design_matrix = create_matrix(gpu_design, 2, 2);
     double test_weights_data[] = {1, 2, 3, 4, 5, 6};
-    matrix* test_weights_matrix = create_matrix(cuda_double_copy_to_gpu(test_weights_data, 6), 2, 3);
+    double* gpu_weights = cuda_double_copy_to_gpu(test_weights_data, 6);
+    matrix* test_weights_matrix = create_matrix(gpu_weights, 2, 3);
     double test_signal_data[] = {1, 2, 3, 4, 5, 6};
-    matrix* test_signal_matrix = create_matrix(cuda_double_copy_to_gpu(test_signal_data, 6), 2, 3);
+    double* gpu_signal = cuda_double_copy_to_gpu(test_signal_data, 6);
+    matrix* test_signal_matrix = create_matrix(gpu_signal, 2, 3);
     double* gpu_result_data = cuda_fitter(test_design_matrix, test_weights_matrix, test_signal_matrix);
     double expected_results[] = {-1, 1, -1, 1, -1, 1};
     double* result_data = cuda_double_return_from_gpu(gpu_result_data, 6);
     CU_ASSERT(true == array_compare(expected_results, result_data, 6, MARGIN));
     free_cuda_memory(gpu_result_data);
+    free_cuda_memory(gpu_design);
+    free_cuda_memory(gpu_weights);
+    free_cuda_memory(gpu_signal);
     free(result_data);
 }
 
