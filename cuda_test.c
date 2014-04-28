fe5c5eb00eab3d568d26c5c381d1406c1d0eb454
Niraj Amalkanti <neji49@gmail.com>
2014-04-28 10:40:35 -0700

    Changed results_tensors to nested pointer

--- cuda_test.c
+++ cuda_test.c
@@ -171,8 +171,8 @@ void test_cuda_decompose_tensors(void){
 
 //Test case for eigendecomposition extraction
 void test_extract_eigendecompositions(void){
-    double eigendecomposition[] = {1, 2, 4, 1, 0, 0, 0, 1, 0, 0, 0, 1, 4, 5, 6, 1, 0, 0, 0, 1, 0, 0, 0, 1};
-    tensor* result_tensors = malloc(sizeof(tensor) * 2);
+    double eigendecomposition[] = {1, 2, 3, 1, 0, 0, 0, 1, 0, 0, 0, 1, 4, 5, 6, 1, 0, 0, 0, 1, 0, 0, 0, 1};
+    tensor** result_tensors = malloc(sizeof(tensor) * 2);
     tensor* first_tensor = malloc(sizeof(tensor));
     tensor* second_tensor = malloc(sizeof(tensor));
     double first_vecs[] = {1, 0, 0, 0, 1, 0, 0, 0, 1};
@@ -183,9 +183,9 @@ void test_extract_eigendecompositions(void){
     matrix* second_tensor_vecs = create_matrix(second_vecs, 3, 3);
     second_tensor->vals = (double[]) {4, 5, 6};
     second_tensor->vecs = second_tensor_vecs;
-    extract_eigendecompositions(eigendecomposition, &result_tensors, 2);
+    extract_eigendecompositions(eigendecomposition, result_tensors, 2);
     CU_ASSERT(compare_tensors(first_tensor, result_tensors, MARGIN));
-    CU_ASSERT(compare_tensors(second_tensor, result_tensors +1, MARGIN));
+    CU_ASSERT(compare_tensors(second_tensor, result_tensors + 1, MARGIN));
     free_tensor(result_tensors);
     free_tensor(result_tensors + 1);
     free(result_tensors);
