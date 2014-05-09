bb56e4b46872e87c99860bf1bb0095b36f5d93db
neji49 <neji49@gmail.com>
2014-05-08 21:05:34 -0700

    Same damn thing

--- cuda_test.c
+++ cuda_test.c
@@ -284,7 +284,7 @@ void test_matrix_weighter (void) {
     double* gpu_weights = cuda_double_copy_to_gpu(weights, 4);
     double* gpu_transpose_test_results = matrix_weighter(gpu_transpose_test_matrix, gpu_weights, 2, 2, 2, true);
     double* transpose_test_result = cuda_double_return_from_gpu(gpu_transpose_test_results, 8);
-    CU_ASSERT(true == array_compare(expected_transpose_matrix, transpose_test_result, 8, MARGIN));
+    CU_ASSERT(true == array_compare(expected_transpose_matrix, transpose_test_results, 8, MARGIN));
     free(transpose_test_result);
     free_cuda_memory(gpu_transpose_test_matrix);
     free_cuda_memory(gpu_weights);
