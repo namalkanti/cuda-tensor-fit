97c93844b0838e900c0341ed57f2e19be65867f1
neji49 <neji49@gmail.com>
2014-05-08 21:11:31 -0700

    Adding back non transposed weighting

--- cuda_test.c
+++ cuda_test.c
@@ -280,10 +280,13 @@ void test_matrix_weighter (void) {
     double weights[] = {1, 2, 3, 4};
     double expected_matrix[] = {1, 4, 3, 8, 3, 8, 9, 16};
     double expected_transpose_matrix[] = {1, 2, 6, 8, 3, 6, 12, 16};
+    double* gpu_test_matrix = cuda_double_copy_to_gpu(test_matrix, 4);
     double* gpu_transpose_test_matrix = cuda_double_copy_to_gpu(transpose_test_matrix, 4);
     double* gpu_weights = cuda_double_copy_to_gpu(weights, 4);
+    double* gpu_test_results = matrix_weighter(gpu_test_matrix, gpu_weights, 2, 2, 2, false);
     double* gpu_transpose_test_results = matrix_weighter(gpu_transpose_test_matrix, gpu_weights, 2, 2, 2, true);
     double* transpose_test_results = cuda_double_return_from_gpu(gpu_transpose_test_results, 8);
+    CU_ASSERT(true == array_compare(expected_matrix, test_results, 8, MARGIN));
     CU_ASSERT(true == array_compare(expected_transpose_matrix, transpose_test_results, 8, MARGIN));
     free(transpose_test_results);
     free_cuda_memory(gpu_transpose_test_matrix);
