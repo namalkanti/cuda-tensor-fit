5a8f9376a52d7392543baac34e8554ed3dd43fa8
neji49 <neji49@gmail.com>
2014-05-22 21:17:51 -0700

    Removing debug check

--- cuda_util.cu
+++ cuda_util.cu
@@ -75,7 +75,6 @@ double* cuda_fitter(matrix const* design_matrix, matrix const* column_major_weig
     double* weighted_design_data = matrix_weighter(design_matrix->data, column_major_weights->data, 
             design_matrix->rows, design_matrix->columns, column_major_weights->rows, true);
     double* solution_vectors;
-    double* debug_weight_check = cuda_double_return_from_gpu(weighted_design_data, design_matrix->rows * design_matrix->columns * signals->columns);
     int signal_elements = signals->rows * signals->columns;
     cuda_double_allocate(&solution_vectors, signal_elements);
     int solver_status = dsolve_batch(weighted_design_data, signals->data, solution_vectors, 
