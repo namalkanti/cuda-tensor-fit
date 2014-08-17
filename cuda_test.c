cb615a95835b25798fa56ece8b70d378940c2921
Niraj Amalkanti <neji49@gmail.com>
2014-08-16 23:59:32 -0700

    Added dot matrices test

--- cuda_test.c
+++ cuda_test.c
@@ -390,6 +390,11 @@ int main(){
         return CU_get_error();
     }
 
+    if ((NULL == CU_add_test(cuda_suite, "Dot matrix test", test_dot_matrices))){
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
     CU_basic_set_mode(CU_BRM_VERBOSE);
     CU_basic_run_tests();
     CU_cleanup_registry();
