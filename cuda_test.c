5e8e3d237b1079a75c501fcbdc2ab059984a93e3
Niraj Amalkanti <neji49@gmail.com>
2014-08-17 21:38:03 -0700

    Added test_fit_signal to cunit test

--- cuda_test.c
+++ cuda_test.c
@@ -401,6 +401,11 @@ int main(){
         return CU_get_error();
     }
 
+    if ((NULL == CU_add_test(cuda_suite, "Fit complete signal test", test_fit_signal))){
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
     CU_basic_set_mode(CU_BRM_VERBOSE);
     CU_basic_run_tests();
     CU_cleanup_registry();
