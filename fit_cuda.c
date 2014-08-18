c1aea91cbfeddfd9a0df28617d85f424d284b1ed
Niraj Amalkanti <neji49@gmail.com>
2014-08-17 21:34:46 -0700

    Changed argument name to cuda_fitter to correct name

--- fit_cuda.c
+++ fit_cuda.c
@@ -5,7 +5,7 @@ void fit_complete_signal(matrix* ols_fit, matrix* design_matrix, matrix* signal,
     matrix* processed_signal_gpu = process_signal(signal, min_signal);
     matrix* column_major_weights_gpu = generate_weights(ols_fit, processed_signal_gpu);
     matrix* column_major_design_matrix_gpu = process_matrix(design_matrix);
-    double* tensors_gpu = cuda_fitter(column_major_design_matrix_gpu, column_major_weights, 
+    double* tensors_gpu = cuda_fitter(column_major_design_matrix_gpu, column_major_weights_gpu, 
             processed_signal_gpu);
     double* padded_eigendecompositions_gpu = cuda_decompose_tensors(tensors_gpu);
     extract_eigendecompositions(padded_eigendecompositions_gpu, tensor_output, signal->columns);
