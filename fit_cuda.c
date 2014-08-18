e978ff1c76fe2c3f3595c170b254870c1a4c6875
Niraj Amalkanti <neji49@gmail.com>
2014-08-17 21:50:08 -0700

    created a copy of signal that isn't on the gpu

--- fit_cuda.c
+++ fit_cuda.c
@@ -5,7 +5,8 @@ void fit_complete_signal(matrix* ols_fit, matrix* design_matrix, matrix* signal,
     int number_of_signals = signal->rows;
     int signal_elements = signal->columns;
     matrix* processed_signal_gpu = process_signal(signal, min_signal);
-    matrix* column_major_weights_gpu = generate_weights(ols_fit, processed_signal_gpu);
+    signal->data = cuda_double_return_from_gpu(process_matrix->data, number_of_signals * signal_elements);
+    matrix* column_major_weights_gpu = generate_weights(ols_fit, signal);
     matrix* column_major_design_matrix_gpu = process_matrix(design_matrix);
     double* tensors_gpu = cuda_fitter(column_major_design_matrix_gpu, column_major_weights_gpu, 
             processed_signal_gpu);
