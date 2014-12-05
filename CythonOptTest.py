cc27c96c099261dc4e9b714297e8673a03ad4995
neji49 <neji49@gmail.com>
2014-12-05 08:28:46 -0800

    Adding python unittest

---
+++ CythonOptTest.py
@@ -0,0 +1,41 @@
+import os.path
+import unittest 
+
+import numpy as np
+import scipy as sp
+
+from cython_opt import tensor_fit
+
+#Test info
+DATA = "test_data"
+SIGNALS = "signal.npy"
+OLS = "ols_fit.npy"
+DESIGN = "design_matrix.npy"
+MINS = "mins.npy"
+EXPECTED = "tensors.npy" 
+
+FILES = [SIGNALS, OLS, DESIGN, MINS, EXPECTED]
+
+class CythonOptTest(unittest.TestCase):
+    """
+    Unit tests for tensor fit c extension.
+    """
+
+    def setUp(self):
+        """
+        Initializes arrays
+        """
+        self._signals, self._ols, self._design, self._mins, self._expected = [np.load(os.path.join(DATA, file_name)) for file_name in FILES]
+    def test_tensor_fit(self):
+        """
+        Tests tensor fit function.
+        """
+
+        min_signal = self._mins[0]
+        min_diffusivity = self._mins[1]
+        result = tensor_fit(self._ols, self._design, self._signals, min_signal, min_diffusivity)
+
+        self.assertTrue(np.allclose(self._expected, result))
+
+if __name__ == "__main__":
+    unittest.main()
