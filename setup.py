7df067b15428359f847e50f7997ba0b2f9627202
neji49 <neji49@gmail.com>
2014-12-05 08:54:59 -0800

    Removed debugging info from setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from distutils.core import setup
 from distutils.extension import Extension
+
+from Cython.Build import cythonize
 from Cython.Distutils import build_ext
+
 from numpy import get_include
 
 ext_modules = [Extension("cython_opt",
@@ -10,7 +13,7 @@ ext_modules = [Extension("cython_opt",
                                 "opt_util.c",
                                 "structure_util.c"],
                     include_dirs=[".", get_include()],
-                    extra_compile_args=["-fopenmp"],
+                    extra_compile_args=["-fopenmp", "-g"],
                     extra_link_args=["-lm", "-lgsl", "-lgslcblas", "-fopenmp"])]
 
 setup(
