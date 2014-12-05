520e6817c99c2ed54bc802500179d19c02fb0619
neji49 <neji49@gmail.com>
2014-12-05 10:03:37 -0800

    Adding coercing to float for accuracy

--- cython_opt.pyx
+++ cython_opt.pyx
@@ -15,6 +15,7 @@ def tensor_fit(ols_fit, design_matrix, signal, float min_signal, float min_diffu
     ols_columns = ols_fit.shape[1]
     design_rows = design_matrix.shape[0]
     design_columns = design_matrix.shape[1]
+    signal = signal.astype(float)
     signals = signal.shape[0]
     signal_elements = signal.shape[1]
 
