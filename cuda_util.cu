50df60ef51b1477e6cb465e3316b0aaba243a9cd
Niraj Amalkanti <neji49@gmail.com>
2015-08-04 17:29:16 -0700

    Updating transpose weighting kernel with same switch

--- cuda_util.cu
+++ cuda_util.cu
@@ -470,9 +470,9 @@ __global__ void weighting_kernel (double const* matrices, double const* weights,
 //kernel for weighting a transposed matrix.
 __global__ void weighting_kernel_transposed(double const* matrices, double const* weights, double* results) {
     int grid_index = blockIdx.x * blockDim.x * blockDim.y;
-    int block_index = blockDim.y * threadIdx.y + threadIdx.x;
+    int block_index = blockDim.y * threadIdx.x + threadIdx.y;
     int matrix_index = grid_index + block_index;
-    int weighting_index = blockIdx.x * blockDim.y + threadIdx.y; 
+    int weighting_index = blockIdx.x * blockDim.x + threadIdx.x; 
     results[matrix_index] = matrices[block_index] * weights[weighting_index];
 }
 
