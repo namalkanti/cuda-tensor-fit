94bd3929c6601696b07777e7eb64392ce82decaa
Niraj Amalkanti <neji49@gmail.com>
2015-08-04 17:27:57 -0700

    Also updating weight index to take into account indexing

--- cuda_util.cu
+++ cuda_util.cu
@@ -463,7 +463,7 @@ __global__ void weighting_kernel (double const* matrices, double const* weights,
     int matrix_grid_index = blockIdx.x * blockDim.x * blockDim.y;
     int block_index = blockDim.y * threadIdx.x + threadIdx.y;
     int matrix_index = matrix_grid_index + block_index;
-    int weight_index = blockIdx.x * blockDim.x + threadIdx.x; 
+    int weight_index = blockIdx.x * blockDim.y + threadIdx.y; 
     results[matrix_index] = matrices[block_index] * weights[weight_index];
 }
 
