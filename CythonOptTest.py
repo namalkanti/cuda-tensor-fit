f67dfe74ebb0c2a8195679a1aa84153eae264820
Niraj Amalkanti <neji49@gmail.com>
2015-08-28 02:13:33 +0000

    Changing CythonOpt to only check eigenvalues

--- CythonOptTest.py
+++ CythonOptTest.py
@@ -26,6 +26,7 @@ class CythonOptTest(unittest.TestCase):
         Initializes arrays
         """
         self._signals, self._ols, self._design, self._mins, self._expected = [np.load(os.path.join(DATA, file_name)) for file_name in FILES]
+
     def test_tensor_fit(self):
         """
         Tests tensor fit function.
@@ -34,8 +35,10 @@ class CythonOptTest(unittest.TestCase):
         min_signal = self._mins[0]
         min_diffusivity = self._mins[1]
         result = tensor_fit(self._ols, self._design, self._signals, min_signal, min_diffusivity)
+        expected_eigs = np.array([np.array(arr[0:3]) for arr in self._expected])
+        result_eigs = np.array([np.array(arr[0:3]) for arr in result])
 
-        self.assertTrue(np.allclose(self._expected, result))
+        self.assertTrue(np.allclose(expected_eigs, result_eigs))
 
 if __name__ == "__main__":
     unittest.main()
