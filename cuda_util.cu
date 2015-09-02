2678f05134639916a4cde912de13dcdfdc81f9ea
Niraj Amalkanti <neji49@gmail.com>
2015-09-02 00:17:48 -0400

    Added freeing of intermediate arrays

--- cuda_util.cu
+++ cuda_util.cu
@@ -186,6 +186,8 @@ double* cuda_fitter(matrix const* design_matrix, matrix const* column_major_weig
     for(i = 0;i < batch_size;i++){
         sol_offset = i * design_matrix->columns ;
         gpu_error_check(cudaMemcpy(results + sol_offset, sol_array[i], sizeof(double) * design_matrix->columns, cudaMemcpyDeviceToDevice));
+        free_cuda_memory(design_inter[i]);
+        free_cuda_memory(sol_inter[i])
     }
 
     status = cublasDestroy_v2(handle);
