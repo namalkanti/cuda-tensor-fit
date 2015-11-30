60abc99f7288ed42284ec2d6409ae061e91ba1a6
neji49 <neji49@gmail.com>
2015-11-29 23:44:41 -0500

    Changing line 39 ot use DTYPE variable

--- cython_opt.pyx
+++ cython_opt.pyx
@@ -36,7 +36,7 @@ def tensor_fit(ols_fit, design_matrix, signal, float min_signal, float min_diffu
     ols_columns = ols_fit.shape[1]
     design_rows = design_matrix.shape[0]
     design_columns = design_matrix.shape[1]
-    signal = signal.astype(double)
+    signal = signal.astype(DTYPE)
     signals = signal.shape[0]
     signal_elements = signal.shape[1]
 
